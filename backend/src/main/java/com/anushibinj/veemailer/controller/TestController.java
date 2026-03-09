d5168956e9d2b0fb6164637c402bc1e7d09e795d
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2026-03-09 11:59:12 +0530

    ✨ Hardcoded query wonderfully works

--- backend/src/main/java/com/anushibinj/veemailer/controller/TestController.java
+++ backend/src/main/java/com/anushibinj/veemailer/controller/TestController.java
@@ -26,23 +26,47 @@ public class TestController {
 
 	@GetMapping
 	public List<EntityModel> test() {
-		Octane octaneClient = octaneCacheService.getOctaneClient(
-				valueEdgeProperties.getServerUrl(),
-				valueEdgeProperties.getClientId(),
-				valueEdgeProperties.getClientSecret(),
-				valueEdgeProperties.getSharedSpaceId(),
-				valueEdgeProperties.getWorkspaceId());
+		Octane octaneClient = octaneCacheService.getOctaneClient(valueEdgeProperties.getServerUrl(),
+				valueEdgeProperties.getClientId(), valueEdgeProperties.getClientSecret(),
+				valueEdgeProperties.getSharedSpaceId(), valueEdgeProperties.getWorkspaceId());
 
-		final Query.QueryBuilder idQueryBuilder = Query.statement("subtype", QueryMethod.EqualTo, "defect")
-//				.andNot(
-//				"phase", QueryMethod.In,
-//				new String[] { "phase.defect.closed", "phase.feature.done", "phase.quality_story.done",
-//						"phase.epic.done", "phase.story.done", "phase.defect.fixed", "phase.defect.rejected",
-//						"phase.defect.duplicate", "phase.defect.deferred" })
+		final Query.QueryBuilder subtypeQuery = //
+				Query.statement("subtype", QueryMethod.In, new String[] { //
+						"defect" //
+				}) //
 		;
-		final Query query = idQueryBuilder.build();
 
-		OctaneCollection<EntityModel> execute = octaneClient.entityList("work_items").get().query(query).execute();
+		final Query.QueryBuilder defectTypeQuery = Query.statement("defect_type", QueryMethod.EqualTo,
+				Query.statement("id", QueryMethod.In, //
+						new String[] { //
+								"pgxw2gl93dd60aldlqq5w7596" //
+						}));
+
+		final Query.QueryBuilder productUdfQuery = Query.statement("product_udf", QueryMethod.EqualTo,
+				Query.statement("id", QueryMethod.In, //
+						new String[] { //
+								"42z3y3le5o36qfqqzv0oxyj5w", //
+								"kozg6dr7lgye3fzm223z8jx2n" //
+						}));
+
+		final Query.QueryBuilder phaseQuery = Query.not("phase", QueryMethod.EqualTo,
+				Query.statement("id", QueryMethod.In, //
+						new String[] { //
+								"phase.defect.closed", //
+								"phase.feature.done", //
+								"phase.quality_story.done", //
+								"phase.epic.done", //
+								"phase.story.done", //
+								"phase.defect.fixed", //
+								"phase.defect.rejected", //
+								"phase.defect.duplicate", //
+								"phase.defect.deferred" //
+						}));
+
+		final Query query = subtypeQuery.and(defectTypeQuery).and(productUdfQuery).and(phaseQuery).build();
+
+		OctaneCollection<EntityModel> execute = octaneClient.entityList("work_items").get().query(query)
+				.addFields("id", "global_id_udf", "name", "phase", "owner", "product_udf", "customer_udf").execute();
 		List<EntityModel> list = execute.stream().toList();
 		return list;
 	}
